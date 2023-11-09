919b72c967b270b3546b03940229debc8d9838fc
Aleksey Kostov <aleksey_kostov@abv.bg>
2023-11-09 14:05:33 +0200

    SoftUni OOP tasks

--- lab_static_and_class_methods/03_integer.py
+++ lab_static_and_class_methods/03_integer.py
@@ -1,25 +1,40 @@
-from functools import reduce
+class Integer:
+    def __init__(self, value: int):
+        self.value = value
+
+    @classmethod
+    def from_float(cls, float_value):
+        if not isinstance(float_value, float):
+            return "value is not a float"
+        return cls(int(float_value))
+
+    @classmethod
+    def from_roman(cls, value):
+        roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        num = 0
+
+        for i in range(len(value)):
+            if i != len(value) - 1 and roman[value[i]] < roman[value[i + 1]]:
+                num += roman[value[i]] * -1
+            else:
+                num += roman[value[i]]
+        return cls(num)
+
+    @classmethod
+    def from_string(cls, value):
+        if not isinstance(value, str):
+            return 'wrong type'
+        try:
+            return cls(int(value))
+        except ValueError:
+            return "wrong type"
 
 
-class Calculator:
+first_num = Integer(10)
+print(first_num.value)
 
-    @staticmethod
-    def add(*args):
-        return reduce(lambda x, y: x + y, args)
+second_num = Integer.from_roman("IV")
+print(second_num.value)
 
-    @staticmethod
-    def multiply(*args):
-        return reduce(lambda x, y: x * y, args)
-
-    @staticmethod
-    def divide(*args):
-        return reduce(lambda x, y: x / y, args)
-
-    @staticmethod
-    def subtract(*args):
-        return reduce(lambda x, y: x - y, args)
-
-print(Calculator.add(5, 10, 4))
-print(Calculator.multiply(1, 2, 3, 5))
-print(Calculator.divide(100, 2))
-print(Calculator.subtract(90, 20, -50, 43, 7))
+print(Integer.from_float("2.6"))
+print(Integer.from_string(2.6))
