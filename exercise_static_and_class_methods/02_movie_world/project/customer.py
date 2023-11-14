04f15ff1d88c624a36837ce5438971cfd2c2c0e2
Aleksey Kostov <aleksey_kostov@abv.bg>
2023-11-14 21:58:02 +0200

    SoftUni OOP tasks

--- exercise_static_and_class_methods/02_movie_world/project/customer.py
+++ exercise_static_and_class_methods/02_movie_world/project/customer.py
@@ -2,7 +2,7 @@ from typing import List
 from project.dvd import DVD
 
 
-class Costumer:
+class Customer:
     def __init__(self, name: str, age: int, costumer_id: int):
         self.name = name
         self.age = age
@@ -11,5 +11,5 @@ class Costumer:
 
     def __repr__(self):
         dvd_names = ", ".join([d.name for d in self.rented_dvds])
-        return (f"{id}: {self.name} of age {self.age} has {len(self.rented_dvds)}"
+        return (f"{self.id}: {self.name} of age {self.age} has {len(self.rented_dvds)}"
                 f" rented DVD's ({dvd_names})")
--- exercise_static_and_class_methods/02_movie_world/project/dvd.py
+++ exercise_static_and_class_methods/02_movie_world/project/dvd.py
@@ -10,3 +10,24 @@ class DVD:
     @classmethod
     def from_date(cls, dvd_id: int, name: str, date: str, age_restriction: int):
         month, year = [int(x) for x in date.split(".")[1:]]
+        months = {
+            1: "January",
+            2: "February",
+            3: "March",
+            4: "April",
+            5: "May",
+            6: "June",
+            7: "July",
+            8: "August",
+            9: "September",
+            10: "October",
+            11: "November",
+            12: "December"
+        }
+        return cls(name, dvd_id, year, months[month], age_restriction)
+
+    def __repr__(self):
+        return (f"{self.id}: {self.name} ({self.creation_month} {self.creation_year}) "
+                f"has age restriction {self.age_restriction}. "
+                f"Status: {'not ' if not self.is_rented else ''}rented")
+    
