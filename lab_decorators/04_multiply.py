10a6a65b1375c96ae0651bfbb3ac9537f34f12df
Aleksey Kostov <aleksey_kostov@abv.bg>
2023-11-25 16:01:48 +0200

    SoftUni OOP tasks

--- lab_decorators/04_multiply.py
+++ lab_decorators/04_multiply.py
@@ -3,17 +3,23 @@ def multiply(times):
         def wrapper(*args, **kwargs):
             result = function(*args, **kwargs)
             return result * times
+
         return wrapper
+
     return decorator
+
 
 @multiply(3)
 def add_ten(number):
     return number + 10
 
+
 print(add_ten(3))
+
 
 @multiply(5)
 def add_ten(number):
     return number + 10
+
 
 print(add_ten(6))
