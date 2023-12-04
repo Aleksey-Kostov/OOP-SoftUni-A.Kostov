c110e3017560c4fae7aa50600ce15e4331a16bd0
Aleksey Kostov <aleksey_kostov@abv.bg>
2023-12-04 15:56:53 +0200

    SoftUni OOP tasks

--- old_exam/problem2/project/services/base_service.py
+++ old_exam/problem2/project/services/base_service.py
@@ -2,10 +2,18 @@ from abc import ABC, abstractmethod
 
 
 class BaseService(ABC):
+
+    ERROR_MSG_NAME = "Service name cannot be empty!"
+    ERROR_MSG_CAPACITY = "Service capacity cannot be less than or equal to 0!"
+
     def __init__(self, name: str, capacity: int):
         self.name = name
         self.capacity = capacity
         self.robots = []
+
+    @abstractmethod
+    def details(self):
+        pass
 
     def _get_names(self):
         if not self.robots:
@@ -18,8 +26,8 @@ class BaseService(ABC):
 
     @name.setter
     def name(self, value):
-        if value.strip() == "":
-            raise ValueError("Service name cannot be empty!")
+        if not value.strip():
+            raise ValueError(self.ERROR_MSG_NAME)
         self.__name = value
 
     @property
@@ -29,9 +37,5 @@ class BaseService(ABC):
     @capacity.setter
     def capacity(self, value):
         if value <= 0:
-            raise ValueError("Service capacity cannot be less than or equal to 0!")
+            raise ValueError(self.ERROR_MSG_CAPACITY)
         self.__capacity = value
-
-    @abstractmethod
-    def details(self):
-        pass
